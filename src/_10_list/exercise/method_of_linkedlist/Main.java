688ec8cce3e28f9d24db65d555f6ff2e01cc1039
truongtran2212 <truongtran221298@gmail.com>
2022-04-13 21:50:34 +0700

    up

--- src/_10_list/exercise/method_of_linkedlist/Main.java
+++ src/_10_list/exercise/method_of_linkedlist/Main.java
@@ -2,6 +2,33 @@ package _10_list.exercise.method_of_linkedlist;
 
 public class Main {
     public static void main(String[] args) {
+        NewMyLinkedList newMyLinkedList = new NewMyLinkedList();
+
+        newMyLinkedList.addFirst(10);
+        newMyLinkedList.addLast(15);
+        newMyLinkedList.add(3,4);
+        newMyLinkedList.addFirst(13);
+
+        System.out.println("Độ dài của mảng: " + newMyLinkedList.size());
+
+
+        System.out.println();
+        System.out.println("Mảng gồm các phần tử: ");
+        newMyLinkedList.printList();
+
+        System.out.println("Xét phần tử có trong mảng hay không");
+        System.out.println(newMyLinkedList.contains(3));
+
+        System.out.println("Xóa phần tử");
+        newMyLinkedList.remove(2);
+        newMyLinkedList.printList();
+
+        System.out.println("Vị trí của phần tử trong mảng: ");
+        System.out.println(newMyLinkedList.indexOf(4));
+
+        System.out.println("Lấy ra 1 phần tử");
+//        newMyLinkedList.printList();
+        newMyLinkedList.get(3);
 
     }
 }
--- src/_10_list/exercise/method_of_linkedlist/NewMyLinkedList.java
+++ src/_10_list/exercise/method_of_linkedlist/NewMyLinkedList.java
@@ -1,13 +1,125 @@
 package _10_list.exercise.method_of_linkedlist;
 
-import javax.xml.soap.Node;
-
-public class NewMyLinkedList {
+public class NewMyLinkedList<E> {
     private int numNodes = 0;
     private Node head;
 
     public NewMyLinkedList(Object data) {
-        head = new Node(data) {
-        };
+        head = new Node(data);
+    }
+    public NewMyLinkedList(){
+
+    }
+    private static class Node {
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+    }
+    public void add(int index, Object data) {
+        Node temp = head;
+        Node holder;
+
+        for(int i=0; i < index-1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNodes++;
+    }
+    public void addFirst(Object data) {
+        Node temp = head;
+        head = new Node(data);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public void addLast(Object data) {
+        if (head == null)
+            addFirst(data);
+        else {
+            Node temp = head;
+            while (temp.next != null) temp = temp.next;
+            temp.next = new Node(data);
+        }
+    }
+
+    public void remove(int index) {
+        Node temp = head;
+
+        for (int i = 0; i < index - 1 && temp.next != null; i++) {
+            temp.next = temp.next.next;
+        }
+
+        numNodes--;
+    }
+
+    public void get(int index) {
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) temp = temp.next;
+        System.out.println(temp.data);
+    }
+    public E getFirst() {
+        Node temp = head;
+        return (E) temp.getData();
+    }
+
+    public int size() {
+       return numNodes;
+    }
+
+    public boolean contains(Object data) {
+        Node temp = head;
+        boolean flag = false;
+        for (int i = 0; i <= numNodes; i++) {
+            if ((temp.data).equals(data)) {
+                flag = true;
+                break;
+            }
+            else {
+                flag = false;
+                temp = temp.next;
+            }
+        }
+        return flag;
+    }
+
+    public int indexOf(Object data) {
+        Node temp = head;
+        int index = 0;
+        for (int i = 0; i <= numNodes; i++) {
+            if ((temp.data).equals(data)) {
+                index = i;
+                break;
+            }
+            else {
+                temp = temp.next;
+            }
+        }
+        return index;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+    public void clear() {
+        for (Node temp = head; temp != null; ) {
+            temp = null;
+            temp.data = null;
+            temp = temp.next;
+        }
+        head = null;
+        numNodes = 0;
     }
 }
